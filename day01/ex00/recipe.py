9ff47a00bacd0cb39d153a22f7255798e018dce9
jaejeon <jaejeon@student.42seoul.kr>
2020-05-18 12:53:43 +0900

    Add day01/ex00/recipe.py

--- day01/ex00/recipe.py
+++ day01/ex00/recipe.py
@@ -7,12 +7,25 @@ class Recipe:
     description = ""
 
     def __init__(self, name, cooking_lvl, cooking_time, ingredients, recipe_type, description=""):
+        error_message = ""
         self.name = name
         self.cooking_lvl = cooking_lvl
         self.cooking_time = cooking_time
         self.ingredients = ingredients
         self.recipe_type = recipe_type
         self.description = description
+
+        if cooking_lvl not in range(1, 6):
+            error_message += "`cooking_lvl` must be range 1 to 5. but it is \n"
+        if not isinstance(cooking_time, int) or cooking_time < 0:
+            error_message += "`cooking_time` must be positive numbers in minutes.\n"
+        if not isinstance(ingredients, list):
+            error_message += "`ingredients` must be list.\n"
+        if recipe_type not in ["starter", "lunch", "dessert"]:
+            error_message += "`recipe_type` must be \"starter\", \"lunch\" or \"dessert\".\n"
+
+        if len(error_message) > 0:
+            quit("InputError:\n"+error_message)
 
     def __str__(self):
         """Return the string to print with the recipe info"""
